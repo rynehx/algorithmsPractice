c9f5906828d8fb03ccaf4060caa235f6e4927a47
Henry <rynehx@gmail.com>
2016-06-13 12:49:09 -0700

    added split for 2-3 tree

--- trees/twoThreeST.js
+++ trees/twoThreeST.js
@@ -1,25 +1,25 @@
-var TreeNode = function(key,content){
+var TreeNode = function(key,content,leftChild,rightChild, parent){
 
   this.type = 2;
 
   this.key = key;
   this.content = content;
 
-  this.keyLeft = null;
-  this.contentLeft = null;
-  this.keyRight = null;
-  this.contentRight = null;
+  // this.keyLeft = null;
+  // this.contentLeft = null;
+  // this.keyRight = null;
+  // this.contentRight = null;
 
-  this.parent = null;
-  this.leftChild = null;
-  this.rightChild = null;
-  this.middle = null;
+  this.parent = parent;
+  this.leftChild = leftChild;
+  this.rightChild = rightChild;
+  // this.midChild = null;
 
-  this.midRight = null;
-  this.midLeft = null;
-
-  this.keyMid = null;
-  this.contentMid = null;
+  // this.midRightChild = null;
+  // this.midLeftChild = null;
+  //
+  // this.keyMid = null;
+  // this.contentMid = null;
 };
 
 
@@ -70,6 +70,7 @@ TreeNode.prototype.addRightVal = function(key, content){
 
   this.key = null;
   this.content = null;
+  this.type = 3;
 };
 
 
@@ -81,34 +82,99 @@ TreeNode.prototype.addLeftVal = function(key, content){
 
   this.key = null;
   this.content = null;
+  this.type = 3;
 };
 
 
 TreeNode.prototype.process3 = function(key, content){
-  
-  if(this.leftChild){
 
-    if(key > this.key){
+  if(this.leftChild){//not a leaf so go down the tree
+
+    if(key > this.keyRight){
       this.rightChild.insert(key, content);
-    }else if(key < this.key){
+    }else if(key < this.keyLeft){
       this.leftChild.insert(key, content);
-    }else{
-      //key is equal
+    }else if(key < this.keyRight && key > this.keyLeft){
+      this.midChild.insert(key,content);
     }
 
-  }else{
+  }else{ // hit a leaf node - put the new down and push the updated middle key to the parent
 
-    if(key > this.key){
-      this.addRightVal(key, content);
-    }else if(key < this.key){
-      this.addLeftVal(key, content);
-    }else{
-      //key is equal
+    if(key > this.keyRight){
+      this.keyMid = this.keyRight;
+      this.contentMid = this.contentRight;
+      this.keyRight = key;
+      this.contentRight = content;
+
+    }else if(key < this.keyLeft){
+      this.keyMid = this.keyLeft;
+      this.contentMid = this.contentLeft;
+      this.keyLeft = key;
+      this.contentLeft = content;
+    }else if(key < this.keyRight && key > this.keyLeft){
+      this.keyMid = key;
+      this.contentMid = content;
     }
+
+    this.parent.handleSplit(this);
 
   }
-
 };
+
+
+TreeNode.prototype.handleSplit = function(child){
+//ON SPLIT MAKE SURE TO CHANGE THE TYPE INTO 2!
+
+  if(this.type === 2){ // if the node is 2 branches
+
+    if(this.leftChild === child){//left child
+      this.addLeftVal(child.keyMid,child.contentMid);
+      //right and left child
+      this.midChild = child;
+      this.leftChild =  child.split(this);
+
+      //split!
+
+
+    }else{
+      this.addRightVal(child.keyMid,child.contentMid);
+      //right and left child
+      this.midChild = child.split(this);
+      this.rightChild =  child;
+
+    }
+
+
+  }else{ // if node is 3 branches
+    
+
+    //check parent and recurse
+  }
+  //make into 4 branch node
+  // join the split nodes onto the for branch by checking this (left, mid, or right)
+  //check if parent exists or is 2 or 3 branch
+    //if yes recursive call
+    //if not create a new node attach the split(link as middle child and then split as 2 branch node not recursive)
+};
+
+
+TreeNode.prototype.split = function(parent){
+  var newNode = new TreeNode(this.keyLeft,this.contentLeft,this.leftChild,this.midLeftChild,parent); // new left -- original is right
+  this.key = this.keyRight;
+  this.content = this.contentRight;
+  this.leftChild = this.midRightChild;
+  this.type = 2;
+
+  this.keyRight = undefined;
+  this.keyLeft = undefined;
+  this.midRightChild = undefined;
+  this.midLeftChild = undefined;
+  this.keyMid = undefined;
+  this.contentMid =  undefined;
+
+  return this;
+};
+
 
 
 var TwoThreeST = function(){
