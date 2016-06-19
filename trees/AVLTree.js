6c37eef87ad6732ed302c7ae81857176de0e3ef0
Henry <rynehx@gmail.com>
2016-06-19 02:14:39 -0700

    refactored degree check

--- trees/AVLTree.js
+++ trees/AVLTree.js
@@ -1,23 +1,28 @@
+///AVL TREE DATA STRUCTURE
 
-
-var TreeNode = function(key, content){
+var TreeNode = function(key, content, leaf){
   this.key = key;
   this.content = content;
   this.parent;
-  this.leftChild = this.leaf;
-  this.rightChild = this.leaf;
-  this.degree = 0;
+
+  if(leaf){
+    this.leftChild = leaf;
+    this.rightChild = leaf;
+    this.degree = 0;
+  }else{
+    this.degree = -1;
+  }
 };
 
 
 var AVLTree = function(){
   this.root;
-  this.leaf = new TreeNode(-1,"I am singleton");
+  this.leaf = new TreeNode(null,"I am singleton");
 };
 
-AVLTree.prototype.insert = function(key, content){
-  var newNode =  new TreeNode(key, content);
+AVLTree.prototype.insert = function(key, content){ //AVL tree class method to insert a node
 
+  var newNode =  new TreeNode(key, content, this.leaf);
   if(!this.root){
     this.root = newNode;
     return;
@@ -28,36 +33,53 @@ AVLTree.prototype.insert = function(key, content){
 
 };
 
-TreeNode.insertNode = function(node, newNode){
+TreeNode.insertNode = function(node, newNode){ // tree node helper method for inserting a node
     if(newNode.key < node.key){ //go left
-      if(node.leftChild){
+
+      if(node.leftChild.key){
+
         TreeNode.insertNode(node.leftChild, newNode);
-        if(node.degree <= node.leftChild.degree){
-          node.degree = node.leftChild.degree + 1;
-        }
+        this.checkBalance();
+
       }else{
 
         node.leftChild = newNode;
         node.parent = node;
-        if(node.degree <= node.leftChild.degree){
+        if(node.degree === 0){
           node.degree = 1;
         }
       }
+
     }else{ //go right
-      if(node.rightChild){
+
+      if(node.rightChild.key){
         TreeNode.insertNode(node.rightChild, newNode);
-        if(node.degree <= node.rightChild.degree){
-          node.degree = node.rightChild.degree + 1;
-        }
+        this.checkBalance();
       }else{
 
         node.rightChild = newNode;
         node.parent = node;
-        if(node.degree <= node.rightChild.degree){
+        if(node.degree === 0){
           node.degree = 1;
         }
       }
     }
+};
+
+
+
+
+TreeNode.prototype.checkBalance = function(){ // check if node is balanced to maintain AVL invariant
+  if(Math.abs(this.leftChild.degree - this.rightChild.degree)>1){// subtree is unbalanced -- rebalance here
+
+  }else{//update the height of the subroot
+
+    if(this.leftChild.degree > this.rightChild.degree){
+      this.degree = this.leftChild.degree + 1;
+    }else{
+      this.degree = this.rightChild.degree + 1;
+    }
+  }
 };
 
 
@@ -103,7 +125,7 @@ AVLTree.prototype.printTree = function(){
 
 
 var tree = new AVLTree();
-for(var i = 0; i <= 300; i++){
+for(var i = 0; i <= 10; i++){
   tree.insert(Math.floor(Math.random()*100),"I got put in order " + i );
 }
 
