6bf68164229b3d52b0bf4f26703570074af1e999
Henry <rynehx@gmail.com>
2016-06-24 20:21:52 -0700

    finished tries

---
+++ trees/trie.js
@@ -0,0 +1,126 @@
+var Trie = function(){
+  this.root = new Node(null);
+};
+
+var Node = function(letter,term){
+  this.letter = letter;
+  this.children= {};
+  this.term = term;
+};
+
+Trie.prototype.inject = function(word){
+  var current = this.root;
+  for(var i =0; i< word.length;i++){
+
+    if(i===word.length-1){
+      if(current.children[word[i]]){
+        current.children[word[i]].term = true;
+      }else{
+        current.children[word[i]]= new Node(word[i],true);
+      }
+    }
+
+
+    if(!current.children[word[i]]){
+      current.children[word[i]]= new Node(word[i]);
+    }
+
+    current = current.children[word[i]];
+
+  }
+};
+
+
+Trie.prototype.searches = function(word){
+  // var current = this.root;
+  // for(var i =0; i< word.length;i++){
+  //
+  //   if(i===word.length-1){
+  //     if(!current.children[word[i]].term){
+  //       return false;
+  //     }
+  //   }
+  //
+  //
+  //   if(current.children[word[i]]){
+  //     current = current.children[word[i]];
+  //   }else{
+  //     return false;
+  //   }
+  // }
+  // return true;
+  var node = this.navigates(word);
+
+  if(node.term){
+    console.log(word + " exists");
+  }else{
+    console.log(word + " doesn't exist");
+  }
+
+};
+
+
+Trie.prototype.delete = function(word){
+
+  // var current = this.root;
+  // for(var i =0; i< word.length;i++){
+  //
+  //   if(i===word.length-1){
+  //     if(current.children[word[i]].term){
+  //       current.children[word[i]].term = false;
+  //     }
+  //   }
+  //
+  //
+  //   if(current.children[word[i]]){
+  //     current = current.children[word[i]];
+  //   }else{
+  //     return false;
+  //   }
+  // }
+  var node = this.navigates(word);
+
+  if(node.term){
+    node.term = false;
+    console.log(word + " deleted");
+  }else{
+    console.log(word + " doesn't exist");
+  }
+
+};
+
+
+Trie.prototype.navigates = function(word){
+  var current = this.root;
+  for(var i =0; i< word.length;i++){
+
+    if(current.children[word[i]]){
+      current = current.children[word[i]];
+    }else{
+      return false;
+    }
+
+  }
+
+  return current;
+};
+
+
+
+var dictionary = new Trie();
+dictionary.inject("brooms");
+dictionary.inject("broom");
+dictionary.inject("bro");
+dictionary.inject("broomses");
+dictionary.inject("broomseewrwe");
+dictionary.searches("brooms");
+dictionary.delete("brooms");
+
+dictionary.searches("brooms");
+dictionary.delete("brooms");
+dictionary.searches("broom");
+dictionary.searches("broomses");
+dictionary.delete("bro");
+dictionary.searches("bro");
+dictionary.delete("broomses");
+dictionary.searches("broomses");
